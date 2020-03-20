297420842aef19f9b6c403865522a4924205f94d
liuyudan <1586456863@qq.com>
2020-03-20 15:44:06 +0800

    移除CORS

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
 
 export default {
   name: 'App',
@@ -18,13 +17,7 @@ export default {
     return{
       age:30
     }
-  },
-  mounted(){
-    let url="https://ayuteam.cn/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
-    axios.get(url).then(()=>{
-
-    })
-  }
+  }  
 }
 </script>
 
